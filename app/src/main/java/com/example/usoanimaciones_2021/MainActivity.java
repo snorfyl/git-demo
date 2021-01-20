2d18783e0357c1f908f553d41ea6a820a5c09b3e
snorfyl <unicramio@hotmail.com>
2021-01-20 01:35:23 +0100

    first git

---
+++ app/src/main/java/com/example/usoanimaciones_2021/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.usoanimaciones_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity  {
+
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}

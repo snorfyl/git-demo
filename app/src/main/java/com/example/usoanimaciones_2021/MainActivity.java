da2146bc4bc48ebbd4b6937903138f089a2f4084
snorfyl <unicramio@hotmail.com>
2021-01-20 01:46:03 +0100

    first git

--- app/src/main/java/com/example/usoanimaciones_2021/MainActivity.java
+++ app/src/main/java/com/example/usoanimaciones_2021/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usoanimaciones_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.e("Prueba","Esto es una prueba");
 
     }
 
